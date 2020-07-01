4f1fe8a2f36ba8ea0b543a26fb37d638eb19da6c
haenara.shin <haenara.shin@gmail.com>
2020-07-01 13:10:44 +0900

    Create SeriesNumMaxSum.py

---
+++ SeriesNumMaxSum.py
@@ -0,0 +1,26 @@
+import sys
+
+def getSubsum(data) :
+    '''
+    n개의 숫자가 list로 주어질 때, 그 연속 부분 최대합을 반환하는 함수를 작성하세요.
+    '''
+    sum_num = []
+    for i in range(len(data)):
+        for j in range(i,len(data)):
+            sum_num.append(sum(data[i:j]))
+
+    return max(sum_num)
+
+def main():
+    '''
+    이 부분은 수정하지 마세요.
+    '''
+
+    data = [int(x) for x in input().split()]
+
+    print(getSubsum(data))
+
+if __name__ == "__main__":
+    main()
+
+
