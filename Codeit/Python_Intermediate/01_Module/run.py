475bda149afcd219bfc40aaa58628c0032670c23
haenara.shin <has163@ucsd.edu>
2021-03-14 22:43:07 -0700

    Add files via upload

---
+++ Codeit/Python_Intermediate/01_Module/run.py
@@ -0,0 +1,13 @@
+import area
+
+print(area.circle(3))
+print(area.square(3))
+print(area.PI)
+
+import shape2d
+
+circle_area = shape2d.Circle(2)
+print(circle_area.area())
+
+square_area = shape2d.Square(2)
+print(square_area.area())
